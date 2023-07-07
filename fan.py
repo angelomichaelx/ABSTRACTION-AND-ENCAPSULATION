95540478eccf386c2c9524df637000f131d8af38
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-07-07 16:09:25 +0800

    (Getter and Setter) for color

--- fan.py
+++ fan.py
@@ -33,7 +33,12 @@ class Fan:
     def set_radius(self, radius):
           self.__radius = radius
     
+    def set_color(self):
+          return self.__color
 
+    def set_color(self, color):
+          self.__color = color
+    
     
     
     
