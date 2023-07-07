1edde96fc1c8198dced2d219bff9bf4d2130025f
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-07-07 19:16:07 +0800

    brake 1st to  5th call and adding some colordesign

--- cartest.py
+++ cartest.py
@@ -10,30 +10,47 @@ class testcar:
 
 #result
         #car info
-        print("=" * 15)
+        print("<=>" * 5)
         print("YEAR:", car.get_yearmodel())
         print("MODEL:", car.get_make())
         print("SPEED:", car.get_speed())
-        print("=" * 15)
+        print("<=>" * 5)
         print()
         
         #accelerate       
         print("\033[1;32mAccelerating *BROOM*")
         #acceleraion 1st call
-        print("\033[1;32mSpeed:", car.get_speed())
+        print("\033[1;32mCurrent Speed:", car.get_speed())
         #acceleraion 2nd call
         car.accelerate()
-        print("\033[1;32mSpeed:", car.get_speed())
+        print("\033[1;32mCurrent Speed:", car.get_speed())
         #acceleraion 3rd call
         car.accelerate()
-        print("\033[1;32mSpeed:", car.get_speed())
+        print("\033[1;32mCurrent Speed:", car.get_speed())
         #acceleraion 4th call
         car.accelerate()
-        print("\033[1;32mSpeed:", car.get_speed())
+        print("\033[1;32mCurrent Speed:", car.get_speed())
         #acceleraion 5th call
         car.accelerate()
-        print("\033[1;32mSpeed:", car.get_speed())   
+        print("\033[1;32mCurrent Speed:", car.get_speed())   
         print()
-
+        
+        #brake 
+        print("\033[93mBraking")
+        #braking 1st call
+        car.brake()
+        print("\033[93mCurrent Speed:", car.get_speed())
+        #braking 2nd call
+        car.brake()
+        print("\033[93mCurrent Speed:", car.get_speed())
+        #braking 3rd call
+        car.brake()
+        print("\033[93mCurrent Speed:", car.get_speed())
+        #braking 4th call
+        car.brake()
+        print("\033[93mCurrent Speed:", car.get_speed())
+        #braking 5th call
+        car.brake()
+        print("\033[93mCurrent Speed:", car.get_speed())
 run = testcar()
 run.testingcar()
