9df7039691b1897772264c8121f46c5650d610fc
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-07-07 17:47:04 +0800

    final

--- fantest.py
+++ fantest.py
@@ -3,7 +3,31 @@ from fanclass import Fan
 class testfan:
     def runtest(self):
 #two fans
-        fannumber1 = Fan(Fan.FAST, 10, "yellow", True)
-        fannumber2 = Fan(Fan.MEDIUM, 5, "Blue", False)
+        #Fan 1
+        fan1 = Fan(Fan.FAST, True, 10, "Yellow")
+        #Fan 2
+        fan2 = Fan(Fan.MEDIUM, False, 5, "Blue")
 
+#First fan properties
+        print("=" * 15)
+        print("\033[1mFAN NUMBER 1\033[0m") 
+        print("=" * 15) 
+        print("\033[93mFan 1 - Speed:", fan1.get_speed(), "\033[0m")
+        print("\033[93mFan 1 - On:", fan1.is_on(), "\033[0m")
+        print("\033[93mFan 1 - Radius:", fan1.get_radius(), "\033[0m")
+        print("\033[93mFan 1 - Color:", fan1.get_color(), "\033[0m")
+        
+        print("")
+#Second fan properties
+        print("=" * 15)
+        print("\033[1mFAN NUMBER 2\033[0m")
+        print("=" * 15)
+        print("\033[94mFan 2 - Speed:", fan2.get_speed(), "\033[0m")
+        print("\033[94mFan 2 - On:", fan2.is_on(), "\033[0m")
+        print("\033[94mFan 2 - Radius:", fan2.get_radius(), "\033[0m")
+        print("\033[94mFan 2 - Color:", fan2.get_color(), "\033[0m")
+        
 
+# Instance to run testfan 
+test = testfan()
+test.runtest()
