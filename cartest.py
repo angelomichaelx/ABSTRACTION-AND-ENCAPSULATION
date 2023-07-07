807721835a5db361565fe653ff615ed92a726f57
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-07-07 18:52:35 +0800

    car info

--- cartest.py
+++ cartest.py
@@ -3,6 +3,20 @@ from carclass import Car
 
 #create class for testcar
 class testcar:
+    def testingcar(self): 
 
 #car object
-Dodge = Car(2022, "Dodge", 0)
+        car = Car(2022, "Dodge", 0)
+
+#result
+        #car info
+        print("=" * 15)
+        print("YEAR:", car.get_yearmodel())
+        print("MODEL:", car.get_make())
+        print("SPEED:", car.get_speed())
+        print("=" * 15)
+        print()
+        
+
+run = testcar()
+run.testingcar()
