a23227d0865c10fe801a74f3530ae926d17fc6c9
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-07-07 19:44:59 +0800

    input name of pet and adding instance to run test

---
+++ pettest.py
@@ -0,0 +1,18 @@
+#import
+from petsclass import pet
+
+#create petttest class
+class pettest:
+    #define class 
+    def pet_test():
+
+#input name of pet
+        name = input("Enter the name of your pet: ")
+#input type of pet
+#input age of pet
+
+
+
+# Instance to run pettest
+run = pettest
+run.pet_test()
