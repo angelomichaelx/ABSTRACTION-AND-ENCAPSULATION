de3a6b8f3938c455de398b6c338c54898be80f33
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-07-07 15:29:38 +0800

    constructor for fan class

--- fan.py
+++ fan.py
@@ -1,7 +1,9 @@
 # Create fan class
-class fan: 
+class Fan: 
 #3 constants(Slow, Medium and Fast)
-SLOW = 1
-MEDIUM = 2
-FAST = 3
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
 
+#constructor
+    def __init__(self, speed = SLOW, on = False, radius = 5, color = "blue"):
