404abb69d54eb7a95694c108662a05f3904e84b5
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-07-07 17:08:02 +0800

    two fan objects

--- fantest.py
+++ fantest.py
@@ -2,4 +2,8 @@ from fanclass import Fan
 
 class testfan:
     def runtest(self):
-        
+#two fans
+        fannumber1 = Fan(Fan.FAST, 10, "yellow", True)
+        fannumber2 = Fan(Fan.MEDIUM, 5, "Blue", False)
+
+
